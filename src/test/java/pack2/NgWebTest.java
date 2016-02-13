58c8b770c347fff45fc20205c74a64a2573a56ef
Karol <jagla.karol@gmail.com>
2016-02-13 19:54:01 +0100

    dodanie calego projektu

---
+++ src/test/java/pack2/NgWebTest.java
@@ -0,0 +1,42 @@
+package pack2;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NgWebTest {
+    WebDriver driver = new FirefoxDriver();
+
+    @Test(priority = 0)
+    public void startWebdriver() {
+        driver.get("http://www.google.pl");
+        driver.close();
+    }
+
+
+    @Test (priority = 4)
+    public void verifyTitle(){
+     String epxTitle = "Google";
+       Assert.assertEquals(epxTitle,driver.getTitle());
+
+    }
+
+    @Test(priority = 2)
+    public void testMethodOne() {
+        Assert.assertTrue(true);
+    }
+
+    @Test(priority = 3)
+    public void testMethodTwo() {
+        Assert.assertTrue(true);
+    }
+
+    @Test(dependsOnMethods = {"testMethodTwo"})
+    public void testMethodThree() {
+        Assert.assertTrue(true);
+    }
+
+
+}
