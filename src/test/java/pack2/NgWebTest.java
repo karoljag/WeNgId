92ad08cffa36134b81ae3b254c848d7aeabebd67
Your Name <you@example.com>
2016-10-03 12:45:32 +0200

    skelet

--- src/test/java/pack2/NgWebTest.java
+++ src/test/java/pack2/NgWebTest.java
@@ -4,39 +4,50 @@ package pack2;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class NgWebTest {
-    WebDriver driver = new FirefoxDriver();
 
-    @Test(priority = 0)
-    public void startWebdriver() {
-        driver.get("http://www.google.pl");
-        driver.close();
+    private WebDriver driver;
+
+    @BeforeTest
+    public void beforeTest() {
+        driver = new FirefoxDriver();
     }
 
-//
-//    @Test (priority = 4)
-//    public void verifyTitle(){
-//     String epxTitle = "Google";
-//       Assert.assertEquals(epxTitle,driver.getTitle());
-//
-//    }
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
 
     @Test(priority = 2)
     public void testMethodOne() {
         Assert.assertTrue(true);
     }
 
-    @Test(priority = 3)
-    public void testMethodTwo() {
-        Assert.assertTrue(true);
+
+    @Test(priority = 1)
+    public void startWebdriver() {
+        driver.get("http://www.google.pl");
     }
 
-    @Test(dependsOnMethods = {"testMethodTwo"})
-    public void testMethodThree() {
-        Assert.assertTrue(true);
+
+    @Test (priority = 4)
+    public void verifyTitle(){
+     String epxTitle = "Google";
+       Assert.assertEquals(epxTitle,driver.getTitle());
     }
+
+
+
+
+
+
+
+
+
 
 
 }
